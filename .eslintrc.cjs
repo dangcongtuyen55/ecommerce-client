20d6bd62ebed1831db57bec80fc01cd3d064ce8a
dangcongtuyen55 <dangcongtuyen55@gmail.com>
2023-05-13 13:39:41 +0700

    up code

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "react-require"],
+  rules: {
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }], //should add ".ts" if typescript project
+  },
+};
---
+++ src/components/header/Head.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Head = () => {
+  return;
+  <>
+    <h1 className="bg-black">header</h1>
+  </>;
+};
+
+export default Head;
